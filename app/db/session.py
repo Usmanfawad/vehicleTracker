71b23928ed760a31d60f309665445bc10003a477
xAngroN <greb96j@web.de>
2024-04-18 20:22:10 +0200

    increase Database Poolsize

--- app/db/session.py
+++ app/db/session.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_CA_CERT = os.getenv("CA_CERT_PATH")
 if DATABASE_CA_CERT is None:
 
-    engine = create_engine("sqlite:///./sql_app.db", connect_args={"check_same_thread": False})
+    engine = create_engine("sqlite:///./sql_app.db", connect_args={"check_same_thread": False}, pool_size=15, max_overflow=20)
 else:
     DATABASE_URL = os.getenv("DATABASE_URL")
     engine = create_engine(DATABASE_URL, connect_args={
